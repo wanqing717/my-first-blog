e51c40a5c907744b17ab1b9a865a3b1f302c77ba
yjy <13011890505@163.com>
2017-06-08 21:35:07 +0800

    modified templates to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import Post#导入Post模型
+from django.utils import timezone
 
+
+#views是连接数据库中的模型和模板的
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+
 
